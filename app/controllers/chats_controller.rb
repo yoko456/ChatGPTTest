6f3e8f8eb66af7287ad401f5274c2b1a01a6e42f
yoko456 <youko.komuro@gmail.com>
2023-09-23 22:41:19 +0900

    ChatGPTと環境変数がうまくいかない

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,32 @@
+class ChatsController < ApplicationController
+
+  #before_action :set_token, only: :search
+  
+   #   def index
+   #       render :index
+   #   end
+  
+      def index; end
+  
+      def search
+          @client = OpenAI::Client.new(access_token: ENV['API_KEY'])
+          response = @client.chat(
+            parameters: {
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: @query }],
+            })
+      
+          @chats = response.dig("choices", 0, "message", "content")
+        end
+  
+        private
+  
+        def text_params
+          @query = params[:text]
+        end
+  
+   #     def set_token
+   #       @api_key = Rails.application.credentials.dig(:openai, :api_key)
+   #     end
+  end
+  #aaaa
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'chats#index'
+  get 'search', to: 'chats#search'
+
+
 end
